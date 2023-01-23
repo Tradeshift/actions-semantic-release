e64a1f8c792154850c85cdd1ac3426a1a225b807
Jacob Wejendorp <jwe@tradeshift.com>
2023-01-23 09:15:19 +0100

    fix: rewrite to ESM to support v20
    semantic-release v20 ships as ESM, so we need to be compatible.
    Might as well do a rewrite for future.

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
-const path = require('path');
-const util = require('util');
-const exec = util.promisify(require('child_process').exec);
+import path from "path";
+import util from "util";
+import { exec as nodeExec } from "child_process";
+
+const exec = util.promisify(nodeExec);
 
 const run = async () => {
-	// Install Dependencies
-	{
-		const { stdout, stderr } = await exec('npm ci --only=prod --silent', {
-			cwd: path.resolve(__dirname),
-		});
-		console.log(stdout);
-		if (stderr) {
-			return Promise.reject(stderr);
-		}
-	}
+  // Install Dependencies
+  {
+    const __dirname = new URL(".", import.meta.url).pathname;
+    const { stdout, stderr } = await exec("npm ci --only=prod --silent", {
+      cwd: path.resolve(__dirname),
+    });
+    console.log(stdout);
+    if (stderr) {
+      return Promise.reject(stderr);
+    }
+  }
 
-	require('./src/index')();
+  (await import("./src/index.js")).run();
 };
 
-run().catch(console.error);
+run().catch((err) => {
+  process.exitCode = 1;
+  console.error(err);
+});
